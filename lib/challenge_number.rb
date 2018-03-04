b7b540936b63c9a9e06221a4502cac6c80ffaac0
Anthony Tilelli <Anthonyntilelli@users.noreply.github.com>
2018-03-03 23:49:31 -0800

    Updating to meet rubocop standards

--- lib/challenge_number.rb
+++ lib/challenge_number.rb
@@ -1,32 +1,29 @@
-class Challenge_number
-  def initialize (min_num = 0, max_num = min_num + 100)
-    self.MIN = min_num
-    self.MAX = max_num
-    if @MIN < @MAX
-      @number=(@MIN + Random.rand(@MAX))
-    else
-      raise RangeError => "#{@MIN} is not less then #{@MAX}"
-    end
+# Generates random number for user ot guess
+class ChallengeNumber
+  attr_reader :min_num, :max_num
+
+  def initialize(min_num = 0, max_num = min_num + 100)
+    self.min_num = min_num
+    self.max_num = max_num
+    raise RangeError => "#{@min_num} < #{@max_num}" unless @min_num < @max_num
+    @number = (@min_num + Random.rand(@max_num))
   end
 
-  def MIN
-    @MIN
-  end
-  def MAX
-    @MAX
-  end
+  # TODO: Add Guess handling logic
+
   private
+
   def ensure_i(value)
-     raise TypeError => "Not an Integer" unless value.is_a? Integer
+    raise TypeError => 'Not an Integer' unless value.is_a? Integer
   end
-  def MIN=(value)
-      ensure_i(value)
-      @MIN = value
-      @MIN.freeze()
+
+  def min_num=(value)
+    ensure_i(value)
+    @min_num = value
   end
-  def MAX=(value)
-      ensure_i(value)
-      @MAX = value
-      @MAX.freeze()
+
+  def max_num=(value)
+    ensure_i(value)
+    @max_num = value
   end
 end
