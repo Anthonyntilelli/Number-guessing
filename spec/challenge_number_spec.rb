c17d037499da455c1ceda824259a198fd08399ca
Anthony Tilelli <Anthonyntilelli@users.noreply.github.com>
2018-03-27 22:16:26 -0700

    adding describe for method seperate

--- spec/challenge_number_spec.rb
+++ spec/challenge_number_spec.rb
@@ -1,17 +1,24 @@
 require_relative '../lib/challenge_number.rb'
-RSpec.describe 'ChallengeNumber #initialize' do
-  context "when initialize" do
-    subject( :valid ) { ChallengeNumber.new(0, 100) }
-    it "create new game with valid range" do
-      expect(valid.min_num).to eql(0)
-      expect(valid.max_num).to eql(100)
-      expect(valid.tries).to eql(0)
-      expect(valid.win).to eql(nil)
-    end
-    it 'raise error on invalid input' do
-      expect { _invalid_arg = ChallengeNumber.new(0, 'invalid') }.to raise_error(TypeError)
-      expect { _invalid_arg = ChallengeNumber.new(0, 100, 'invalid') }.to raise_error(TypeError)
-      expect { _invalid_range = ChallengeNumber.new(0, -5)}.to raise_error(RangeError)
+RSpec.describe 'ChallengeNumber ' do
+  describe '#initialize' do
+    context 'when initialize' do
+      subject( :valid ) { ChallengeNumber.new(0, 100) }
+      it "create new game with valid range" do
+        expect(valid.min_num).to eql(0)
+        expect(valid.max_num).to eql(100)
+        expect(valid.tries).to eql(0)
+        expect(valid.win).to eql(nil)
+      end
+      it 'raise error on invalid input' do
+        expect { _invalid_arg = ChallengeNumber.new(0, 'invalid') }.to raise_error(TypeError)
+        expect { _invalid_arg = ChallengeNumber.new(0, 100, 'invalid') }.to raise_error(TypeError)
+        expect { _invalid_range = ChallengeNumber.new(0, -5)}.to raise_error(RangeError)
+      end
     end
   end
 end
+
+# guess
+# tell
+# new_game!
+# convert_integer
